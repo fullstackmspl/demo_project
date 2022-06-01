16ffcd09aefb6b91849146a3936baffdbe93fb49
Php__Developer <php.mspl@gmail.com>
2022-06-01 17:30:08 +0530

    lates code

--- permit/auth.js
+++ permit/auth.js
@@ -1,56 +1,59 @@
-'use strict';
+"use strict";
 
-const jwt = require('jsonwebtoken');
-const authConfig = require('config').get('auth');
+const jwt = require("jsonwebtoken");
+const authConfig = require("config").get("auth");
 
 const getToken = (id, isExpired, context) => {
-  const log = context.logger.start(`permit:auth:getToken:${id}`);
+    const log = context.logger.start(`permit:auth:getToken:${id}`);
 
-  const extractFrom = {
-    _id: id,
-  };
+    const extractFrom = {
+        _id: id
+    };
 
-  const options = {};
+    const options = {};
 
-  if (isExpired) {
-    options.expiresIn = '11h';
-  }
+    if (isExpired) {
+        options.expiresIn = "11h";
+    }
 
-  const token = jwt.sign(extractFrom, authConfig.jwtKey, options);
-  log.end();
-  return token;
+    const token = jwt.sign(extractFrom, authConfig.jwtKey, options);
+    log.end();
+    return token;
 };
 const getOtpToken = (otp, id, isExpired, context) => {
-  const log = context.logger.start(`permit:auth:getToken:${id}`);
+    const log = context.logger.start(`permit:auth:getToken:${id}`);
 
-  const extractFrom = {
-    _id: id,
-    otp,
-    otp,
-  };
+    const extractFrom = {
+        _id: id,
+        otp, otp
+    };
 
-  const options = {};
+    const options = {};
 
-  if (isExpired) {
-    options.expiresIn = '4m';
-  }
+    if (isExpired) {
+        options.expiresIn = '4m';
+    }
 
-  const token = jwt.sign(extractFrom, authConfig.jwtKey, options);
-  log.end();
-  return token;
+    const token = jwt.sign(extractFrom, authConfig.jwtKey, options);
+    log.end();
+    return token;
 };
+
 
 const extractToken = (token, context) => {
-  const log = context.logger.start(`permit:auth:requiresToken:${token}`);
-  try {
-    const decoded = jwt.verify(token, authConfig.jwtKey);
-    log.end();
-    return decoded;
-  } catch (err) {
-    log.end();
-    return err;
-  }
+    const log = context.logger.start(`permit:auth:requiresToken:${token}`);
+    try {
+        const decoded = jwt.verify(token, authConfig.jwtKey);
+        log.end();
+        return decoded;
+    } catch (err) {
+        log.end();
+        return err;
+    }
 };
+
+
+
 exports.getToken = getToken;
 exports.extractToken = extractToken;
 exports.getOtpToken = getOtpToken;
